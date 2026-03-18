4070a9c3b45b0f8ca20b8a40bea11d95940b6d89
Raksa <s.raksaruba@gmail.com>
2026-03-18 10:15:38 +0530

    UC1: Initialize Train and Display Consist Summary

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 
+//Initialize the Train Consist Management App and display the initial state of the train.
+//@version 1.0
+public class TrainConsistManagementApp{
+    public static void main(String[] args){
+        System.out.println("==================================");
+        System.out.println("== Train Consist Management App ==");
+        System.out.println("==================================\n");
+        List<String> trainConsist=new ArrayList<>();
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial Bogie Count: "+trainConsist.size());
+        System.out.println("Current Train Consist: "+trainConsist);
+        System.out.println("\nSystem ready for operations...");
+    }
+}
